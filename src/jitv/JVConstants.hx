b02335233a14bebcd2e06437750a172238749503
Fletcher <fletcherc89@gmail.com>
2013-12-08 18:46:49 -0800

    Haxe port begun

---
+++ src/jitv/JVConstants.hx
@@ -0,0 +1,14 @@
+package jitv;
+
+/**
+ * JVConstants
+ * Static class containing game-tuning constants
+ * Created by Fletcher 11/3/2013, Ported by Fletcher 12/8/2013
+ */
+class JVConstants
+{
+	public static inline var FPS:Int = 60;
+	public static inline var ASSUMED_FPS_FOR_PHYSICS:Int = 60;
+	public static inline var BASE_SHIP_MOVEMENT_SPEED:Float = 4.0;
+	public static inline var OFFSCREEN_DELETION_BUFFER:Int = 32;
+}
--- src/jitv/Main.hx
+++ src/jitv/Main.hx
@@ -4,22 +4,21 @@ import com.haxepunk.Engine;
 import com.haxepunk.HXP;
 
 /**
- * ...
+ * Main class for JitV
  * @author Fletcher
  */
-
 class Main extends Engine 
 {
-
+	//TODO - fcole - Figure out we're gonna handle different screen sizes
+	//NOTE - Screen size is also set in application.xml
 	public static inline var kScreenWidth:Int = 640;
-	public static inline var kScreenHeight:Int = 480;
-	public static inline var kFrameRate:Int = 60;
-	public static inline var kClearColor:Int = 0x333333;
+	public static inline var kScreenHeight:Int = 380;
+	public static inline var kClearColor:Int = 0xff7777;
 	public static inline var kProjectName:String = "JitV";
 
 	function new()
 	{
-		super(kScreenWidth, kScreenHeight, kFrameRate, false);	
+		super(kScreenWidth, kScreenHeight, JVConstants.FPS, true);	
 	}
 
 	override public function init()
