87d96bdf1a461e3d506d87c1e3bf063d16ee9943
Fletcher <fletcherc89@gmail.com>
2014-05-11 17:35:11 -0700

    Refactored player ship movement logic to component class

--- src/jitv/entities/JVPlayerShipEntity.hx
+++ src/jitv/entities/JVPlayerShipEntity.hx
@@ -13,6 +13,7 @@ import com.haxepunk.utils.Key;
 import extendedhxpunk.ext.EXTKey;
 import extendedhxpunk.ext.EXTMath;
 import extendedhxpunk.ext.EXTTimer;
+import jitv.entities.components.JVMovementComponent;
 import jitv.entities.components.JVWeaponComponent;
 import jitv.datamodel.persistentdata.JVShipWeapon;
 import jitv.JVConstants;
@@ -36,14 +37,17 @@ class JVPlayerShipEntity extends JVEntity
 		
 		this.mask = new Pixelmask("gfx/masks/player_ship_mask_theme2.png", Std.int(-image.width / 2), Std.int(-image.height / 2));
 		this.type = "player";
-		this._lifeCount = JVConstants.START_LIVES;
-		this._invincible = false;
+		_lifeCount = JVConstants.START_LIVES;
+		_invincible = false;
 		
 		_hud = hud;
 		
 		// Hardcoded gamepad to be 0th one
 		_gamepad = new Gamepad(0);
 		_hud.updateLivesCount(_lifeCount);
+		
+		// Add component for movement
+		this.components.push(new JVMovementComponent(this, _gamepad));
 		
 		// Temporary hardcoded weapons
 		var tempPrimaryWeapon:JVShipWeapon = new JVShipWeapon();
@@ -61,62 +65,6 @@ class JVPlayerShipEntity extends JVEntity
 	override public function update():Void
 	{
 		super.update();
-		
-		// Movement
-		var movementMagnitude:Float = JVConstants.BASE_SHIP_MOVEMENT_SPEED * HXP.elapsed * JVConstants.ASSUMED_FPS_FOR_PHYSICS;
-		
-		var horizontalMovement:Bool = false;
-		var verticalMovement:Bool = false;
-		var xMultiplier:Float = 1.0;
-		var yMultiplier:Float = 1.0;
-		
-		// TODO - Have better checking for controller support
-		if ((Math.abs(_gamepad.leftAnalogueX) > 0) || (Math.abs(_gamepad.leftAnalogueY) > 0))
-		{
-			if (Math.abs(_gamepad.leftAnalogueX) > 0)
-			{
-				horizontalMovement = true;
-				xMultiplier = _gamepad.leftAnalogueX;
-			}
-			if (Math.abs(_gamepad.leftAnalogueY) > 0)
-			{
-				verticalMovement = true;
-				yMultiplier = _gamepad.leftAnalogueY;
-			}
-		}
-		else
-		{
-			if (Input.check(Key.RIGHT))
-			{
-				horizontalMovement = true;
-			}
-			if (Input.check(Key.LEFT))
-			{
-				horizontalMovement = !horizontalMovement;
-				xMultiplier = -1.0;
-			}
-			if (Input.check(Key.DOWN))
-			{
-				verticalMovement = true;
-			}
-			if (Input.check(Key.UP))
-			{
-				verticalMovement = !verticalMovement;
-				yMultiplier = -1.0;
-			}
-		}
-		
-		if (horizontalMovement && verticalMovement)
-		{
-			xMultiplier *= EXTMath.SQRT2_2;
-			yMultiplier *= EXTMath.SQRT2_2;
-		}
-		
-		if (horizontalMovement)
-			this.x += xMultiplier * movementMagnitude;
-		if (verticalMovement)
-			this.y += yMultiplier * movementMagnitude;
-		
 		
 		clampHorizontal(0, JVConstants.PLAY_SPACE_WIDTH);
 		clampVertical(0, JVConstants.PLAY_SPACE_HEIGHT);
@@ -185,8 +133,6 @@ class JVPlayerShipEntity extends JVEntity
 	private var _invincibilityFlashTimer:EXTTimer;
 	private var _invincibilityOffTimer:EXTTimer;
 	
-	private static inline var FIRING_RATE:Float = 0.14;
-	
 	private function toggleVisibility(timer:EXTTimer):Void
 	{
 		visible = !visible;
@@ -197,10 +143,5 @@ class JVPlayerShipEntity extends JVEntity
 		_invincible = false;
 		_invincibilityFlashTimer.invalidate();
 		this.visible = true;
-	}
-	
-	private function updateMovement():Void
-	{
-		
 	}
 }
---
+++ src/jitv/entities/components/JVMovementComponent.hx
@@ -0,0 +1,97 @@
+package jitv.entities.components;
+
+import com.haxepunk.HXP;
+import com.haxepunk.utils.Input;
+import com.haxepunk.utils.Key;
+import extendedhxpunk.io.Gamepad;
+import extendedhxpunk.ext.EXTMath;
+import jitv.entities.JVEntity;
+import jitv.JVConstants;
+
+/**
+ * JVMovementComponent
+ * Handles movement via input for an entity (i.e. the player's ship)
+ * Created by Fletcher, 5/11/2014
+ */
+class JVMovementComponent implements JVEntityComponent
+{
+	public function new(parent:JVEntity, gamepad:Gamepad) 
+	{
+		_parent = parent;
+		_gamepad = gamepad;
+	}
+	
+	public function update():Void 
+	{
+		// Movement
+		var movementMagnitude:Float = JVConstants.BASE_SHIP_MOVEMENT_SPEED * HXP.elapsed * JVConstants.ASSUMED_FPS_FOR_PHYSICS;
+		
+		var horizontalMovement:Bool = false;
+		var verticalMovement:Bool = false;
+		var xMultiplier:Float = 1.0;
+		var yMultiplier:Float = 1.0;
+		
+		// TODO - Have better checking for controller support
+		if ((Math.abs(_gamepad.leftAnalogueX) > 0) || (Math.abs(_gamepad.leftAnalogueY) > 0))
+		{
+			if (Math.abs(_gamepad.leftAnalogueX) > 0)
+			{
+				horizontalMovement = true;
+				xMultiplier = _gamepad.leftAnalogueX;
+			}
+			if (Math.abs(_gamepad.leftAnalogueY) > 0)
+			{
+				verticalMovement = true;
+				yMultiplier = _gamepad.leftAnalogueY;
+			}
+		}
+		else
+		{
+			if (Input.check(Key.RIGHT))
+			{
+				horizontalMovement = true;
+			}
+			if (Input.check(Key.LEFT))
+			{
+				horizontalMovement = !horizontalMovement;
+				xMultiplier = -1.0;
+			}
+			if (Input.check(Key.DOWN))
+			{
+				verticalMovement = true;
+			}
+			if (Input.check(Key.UP))
+			{
+				verticalMovement = !verticalMovement;
+				yMultiplier = -1.0;
+			}
+		}
+		
+		if (horizontalMovement && verticalMovement)
+		{
+			xMultiplier *= EXTMath.SQRT2_2;
+			yMultiplier *= EXTMath.SQRT2_2;
+		}
+		
+		if (horizontalMovement)
+			_parent.x += xMultiplier * movementMagnitude;
+		if (verticalMovement)
+			_parent.y += yMultiplier * movementMagnitude;
+	}
+	
+	public function render():Void 
+	{
+		
+	}
+	
+	public function cleanup():Void 
+	{
+		
+	}
+	
+	/**
+	 * Private
+	 */
+	private var _parent:JVEntity;
+	private var _gamepad:Gamepad;
+}
